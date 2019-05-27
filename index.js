bc654f5798a3522f381745b508dbe326df5c632b
obayomi96 <martinsoluwaseun47@gmail.com>
2019-05-27 17:04:11 +0100

    chore(Express server):initialize Dev script to start the project in development mode (uding nodemon) start script to start the project in production mode (using node) Integrate dotenv for managing environment variables ignore .env in .gitginore Readme.md file updated with instructions on how to run the project WIldcard route to handle any endpoint, return a json message 'hello books deferral' [delivers #166237757]

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import routes from './routes/index';
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false }));
+
+app.use('/api/v1', routes);
+
+app.use('*', (req, res) => {
+    res.status(404).json({
+      msg: 'Opps!!, the page you are looking for cannot be found',
+    });
+  });
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+export default app;
