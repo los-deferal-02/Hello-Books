a5fd976c5a8eae5469674097f6ab3f937a7e15d6
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-05-28 12:07:51 +0100

    chore(eslint): configure eslint for the project - setup ESLint with Airbnb style guide - add .babelrc file - correct errors flagged by ESLint [Finishes #166237752]

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ dotenv.config();
 const port = process.env.PORT || 4000;
 const app = express();
 
+const { log } = console;
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1', routes);
 
 app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Opps!!, the page you are looking for cannot be found',
-    });
+  res.status(404).json({
+    message: 'O0ps!!, the page you are looking for cannot be found',
   });
+});
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  log(`Server started on port ${port}`);
 });
 
 export default app;
