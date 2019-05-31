f1ea7e3f94153dbd1fbc8c70aec03d38fbb3353d
Ayodeji <afolabiayodejia@gmail.com>
2019-05-31 02:53:30 +0100

    Merge with develop

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import routes from './server/routes';
 import swaggerDoc from './hello-books-swagger.json';
+import { errorHandler, error404 } from './server/middlewares/errors';
 
 dotenv.config();
 const port = process.env.PORT || 4000;
@@ -21,12 +22,16 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1', routes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+<<<<<<< HEAD
 
 app.all('/*', (req, res) => {
   res.status(404).json({
     error: 'Oops!! Page not found'
   });
 });
+=======
+app.use(errorHandler, error404);
+>>>>>>> Merge with develop
 
 app.listen(port, () => {
   log(`Server started on port ${port}`);
--- server/helpers/encrypt.js
+++ server/helpers/encrypt.js
@@ -4,15 +4,49 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
+/**
+ *
+ *
+ * @export
+ * @class encrypt
+ */
 export default class encrypt {
+  /**
+   *
+   * Encrypt User Password
+   * @static
+   * @param {string} password
+   * @returns {string} Encrypted Password
+   * @memberof encrypt
+   */
   static encryptPassword(password) {
     return bycrpt.hash(password, 10);
   }
 
+
+  /**
+   *
+   * Decrypt User Password
+   * @static
+   * @param {string} inputPassword
+   * @param {string} encryptedPassword
+   * @returns {boolen} True if valid
+   * @memberof encrypt
+   */
   static decryptPassword(inputPassword, encryptedPassword) {
     return bycrpt.compare(inputPassword, encryptedPassword);
   }
 
+
+  /**
+   *
+   * Generate user token
+   * @static
+   * @param {object} user
+   * @returns {string} Token
+   * @memberof encrypt
+   */
   static generateToken(user) {
     const payload = { id: user.id, email: user.email, role: user.role };
     const token = jwt.sign(payload,
